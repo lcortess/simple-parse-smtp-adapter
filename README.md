bd34213f6ce85c78988a9998e1c2287cef5d7755
Pascal Malbranche <11357019+perzeuss@users.noreply.github.co>
2022-01-05 00:53:15 +0100

    feat: update email-templates and extend functionality

--- README.md
+++ README.md
@@ -19,81 +19,83 @@ This is an example using parse server as express module:
 ```js
 "use strict";
 
-const Express = require('express');
-const ParseServer = require('parse-server').ParseServer;
+const Express = require("express");
+const ParseServer = require("parse-server").ParseServer;
 
 const app = Express();
 const APP_PORT = 1337;
 
-let options = {
-
-};
+let options = {};
 
 let api = new ParseServer({
-	appName: "Parse Test",
-	appId: "12345",
-	masterKey: "abcde12345",
-	serverURL: "http://localhost:1337/parse",
-	publicServerURL: "http://localhost:1337/parse",
-	databaseURI: "mongodb://user:pass@host:27017/parse",
-	port: APP_PORT,
-	//This is the config for email adapter
-	emailAdapter: {
-		module: "simple-parse-smtp-adapter",
-		options: {
-			service:'Gmail', // required
-			clientId:'your_clientid_get_from_console_google_developers',
-            clientSecret:'your_clientsecret_get_from_console_google_developers',
-            refreshToken:'your_refresh_token_get_from_console_google_developers',
-            accessToken:'your_access_token_get_from_console_google_developers',
-			fromAddress: 'your@sender.address',
-			user: 'email@email.com', //#"required for service SMTP"
-			//password: 'AwesomePassword',  //#"required for service SMTP"
-			//host: 'your.smtp.host',  //#"required for service SMTP"
-			//isSSL: true, //True or false if you are using ssl  //#"required for service SMTP"
-			//port: 465, //SSL port or another port //#"required for service SMTP"
-			name: 'your domain name', //  optional, used for identifying to the server 
-			//Somtimes the user email is not in the 'email' field, the email is search first in
-			//email field, then in username field, if you have the user email in another field
-			//You can specify here
-			emailField: 'username', 
-			templates: {
-			    //This template is used only for reset password email
-				resetPassword: {
-				    //Path to your template
-					template: __dirname + '/views/email/reset-password',
-					//Subject for this email
-					subject: 'Reset your password'
-				},
-				verifyEmail: {
-				    template: __dirname + '/views/email/verify-email',
-				    subject: 'Verify Email'
-				}
-			}
-		}
-	}
+  appName: "Parse Test",
+  appId: "12345",
+  masterKey: "abcde12345",
+  serverURL: "http://localhost:1337/parse",
+  publicServerURL: "http://localhost:1337/parse",
+  databaseURI: "mongodb://user:pass@host:27017/parse",
+  port: APP_PORT,
+  //This is the config for email adapter
+  emailAdapter: {
+    module: "simple-parse-smtp-adapter",
+    options: {
+      service: "Gmail", // required
+      clientId: "your_clientid_get_from_console_google_developers",
+      clientSecret: "your_clientsecret_get_from_console_google_developers",
+      refreshToken: "your_refresh_token_get_from_console_google_developers",
+      accessToken: "your_access_token_get_from_console_google_developers",
+      fromAddress: "your@sender.address",
+      user: "email@email.com", //#"required for service SMTP"
+      //password: 'AwesomePassword',  //#"required for service SMTP"
+      //host: 'your.smtp.host',  //#"required for service SMTP"
+      //isSSL: true, //True or false if you are using ssl  //#"required for service SMTP"
+      //port: 465, //SSL port or another port //#"required for service SMTP"
+      name: "your domain name", //  optional, used for identifying to the server
+      //Somtimes the user email is not in the 'email' field, the email is search first in
+      //email field, then in username field, if you have the user email in another field
+      //You can specify here
+      emailField: "username",
+      // Define the extension of your template files, by default handlebars is used.
+      extension: "handlebars",
+      templates: {
+        //This template is used only for reset password email
+        resetPassword: {
+          //Path to your template
+          template: __dirname + "/views/email/reset-password",
+          //Subject for this email
+          subject: "Reset your password",
+        },
+        verifyEmail: {
+          template: __dirname + "/views/email/verify-email",
+          subject: "Verify Email",
+        },
+      },
+    },
+  },
 });
 
 /**
  * Parse Server endpoint
  */
-app.use('/parse', api);
+app.use("/parse", api);
 
 app.listen(APP_PORT, function () {
-	console.log(`Parse Server Ready and listening on port ${APP_PORT}`);
+  console.log(`Parse Server Ready and listening on port ${APP_PORT}`);
 });
 ```
 
 ### Template
-The path you pass to the email adapter must be a directory and not a file, this path must contain 2 mandatory files `html.jade` and `style.less` you can do your template as you like with the [CSS rules that emails supports](https://www.campaignmonitor.com/css/) in the template you can use 3 variables:
+
+The path you pass to the email adapter must be a directory and not a file, this path must contain a file named `index.handlebars`. If you use another template engine, define the adapter option `extension`. For engine sources visit [consolidate.js](https://github.com/tj/consolidate.js/#supported-template-engines) which is the used template engine.
+Inside templates, you can use the following variables:
 
 - appName //This is the name of your parse app
 - link //This is the link for reset the password
 - user //This is a Parse object with the current user, so you can use any field in your User class of parse for example the user name `#{user.get('username')}`
 
 ### Contributing
-This module is pull request friendly in the develop branch feel free of send new features or bug fixes.
 
-If you find a bug please open an issue.
+This module is a private fork and not a maintained public module, however this repo is pull request friendly and all backwards compatible contributions are welcome.
+Free to [open an issue](https://github.com/music-bat/mbat/issues/new) in our main repo. Please also read through the [Code of Conduct](https://github.com/music-bat/mbat/blob/main/CODE_OF_CONDUCT.md) of our [main repo](https://github.com/music-bat/mbat/blob/main/CODE_OF_CONDUCT.md).
 
 ### License MIT
--- index.js
+++ index.js
@@ -1,214 +1,206 @@
 "use strict";
-const path = require('path');
 const nodemailer = require("nodemailer");
-const EmailTemplate = require('email-templates').EmailTemplate;
+const Email = require("email-templates");
 
-let SimpleParseSmtpAdapter = (adapterOptions) => {
-
-    if(!adapterOptions && adapterOptions.service=='Gmail'){
-        if (!adapterOptions ||  
-            !adapterOptions.service|| 
-            !adapterOptions.type ||  
-            !adapterOptions.user || 
-            !adapterOptions.clientId || 
-            !adapterOptions.clientSecret|| 
-            !adapterOptions.refreshToken || 
-            !adapterOptions.accessToken ) {
-            throw 'SimpleParseSMTPAdapter requires service,type, user, clientId,clientSecret,refreshToken and accessToken';
-        }
+let SimpleParseSmtpAdapter = (adapterOptions, b, c) => {
+  if (adapterOptions && adapterOptions.service == "Gmail") {
+    if (
+      !adapterOptions ||
+      !adapterOptions.service ||
+      !adapterOptions.type ||
+      !adapterOptions.user ||
+      !adapterOptions.clientId ||
+      !adapterOptions.clientSecret ||
+      !adapterOptions.refreshToken ||
+      !adapterOptions.accessToken
+    ) {
+      throw "SimpleParseSMTPAdapter requires service,type, user, clientId,clientSecret,refreshToken and accessToken";
     }
-    else if(!adapterOptions && adapterOptions.service=='SMTP'){
-            if (!adapterOptions || !adapterOptions.user || !adapterOptions.password || !adapterOptions.host || !adapterOptions.fromAddress ) {
-                throw 'SimpleParseSMTPAdapter requires user, password, host, and fromAddress';
-            }
-    }else{
-            throw 'SimpleParseSMTPAdapter please choose service Gmail or SMTP';
+  } else if (adapterOptions && adapterOptions.service == "SMTP") {
+    if (
+      !adapterOptions ||
+      !adapterOptions.user ||
+      !adapterOptions.password ||
+      !adapterOptions.host ||
+      !adapterOptions.fromAddress
+    ) {
+      throw "SimpleParseSMTPAdapter requires user, password, host, and fromAddress";
+    }
+  } else {
+    throw "SimpleParseSMTPAdapter please choose service Gmail or SMTP";
+  }
+
+  /**
+   * Creates trasporter for send emails with OAuth2 Gmail
+   */
+  let transporterOAuth2Gmail = nodemailer.createTransport({
+    service: adapterOptions.service,
+    auth: {
+      type: adapterOptions.type,
+      user: adapterOptions.user,
+      clientId: adapterOptions.clientId,
+      clientSecret: adapterOptions.clientSecret,
+      refreshToken: adapterOptions.refreshToken,
+      accessToken: adapterOptions.refreshToken,
+      expires: adapterOptions.expires,
+    },
+  });
+
+  /**
+   * Creates trasporter for send emails with OAuth2 Gmail
+   */
+  let transporter = nodemailer.createTransport({
+    host: adapterOptions.host,
+    port: adapterOptions.port,
+    secure: adapterOptions.isSSL,
+    name: adapterOptions.name || "127.0.0.1",
+    auth: {
+      user: adapterOptions.user,
+      pass: adapterOptions.password,
+    },
+    tls: {
+      rejectUnauthorized:
+        adapterOptions.isTlsRejectUnauthorized !== undefined
+          ? adapterOptions.isTlsRejectUnauthorized
+          : true,
+    },
+  });
+
+  /**
+   * When emailField is defined in adapterOptines return that field
+   * if not return the field email and if is undefined returns username
+   *
+   * @param Parse Object user
+   * @return String email
+   */
+  let getUserEmail = (user) => {
+    let email = user.get("email") || user.get("username");
+
+    if (adapterOptions.emailField) {
+      email = user.get(adapterOptions.emailField);
     }
 
-    /**
-     * Creates trasporter for send emails with OAuth2 Gmail
-     */
-    let transporterOAuth2Gmail = nodemailer.createTransport({
-        service: adapterOptions.service,
-        auth: {
-            type: adapterOptions.type,
-            user: adapterOptions.user,
-            clientId: adapterOptions.clientId,
-            clientSecret: adapterOptions.clientSecret,
-            refreshToken: adapterOptions.refreshToken,
-            accessToken: adapterOptions.refreshToken,
-            expires: adapterOptions.expires
-        }
-    });
+    return email;
+  };
 
-    /**
-     * Creates trasporter for send emails with OAuth2 Gmail
-     */
-     let transporter = nodemailer.createTransport({
-        host: adapterOptions.host,
-        port: adapterOptions.port,
-        secure: adapterOptions.isSSL,
-        name: adapterOptions.name || '127.0.0.1',
-        auth: {
-            user: adapterOptions.user,
-            pass: adapterOptions.password
+  /**
+   * Return an email template with data rendered using email-templates module
+   * check module docs: https://github.com/forwardemail/email-templates
+   *
+   * @param String template path template
+   * @param Object data object with data for use in template
+   */
+  let renderTemplate = (template, data) => {
+    let templateDir = template;
+    let html = new Email({
+      views: {
+        options: {
+          path: templateDir,
+          extension: adapterOptions.extension || "handlebars",
         },
-        tls: {
-            rejectUnauthorized: adapterOptions.isTlsRejectUnauthorized !== undefined ? adapterOptions.isTlsRejectUnauthorized : true
-        }
+      },
     });
 
-    /**
-     * When emailField is defined in adapterOptines return that field
-     * if not return the field email and if is undefined returns username
-     * 
-     * @param Parse Object user
-     * @return String email
-     */
-    let getUserEmail = (user) => {
-        let email = user.get('email') || user.get('username');
+    return html.render(templateDir, data);
+  };
 
-        if (adapterOptions.emailField) {
-            email = user.get(adapterOptions.emailField);
-        }
-
-        return email;
+  /**
+   * Parse use this function by default for sends emails
+   * @param mail This object contain to address, subject and email text in plain text
+   * @returns {Promise}
+   */
+  let sendMail = (mail) => {
+    let mailOptions = {
+      to: mail.to,
+      html: mail.text,
+      subject: mail.subject,
+      from: adapterOptions.fromAddress,
     };
 
-    /**
-     * Return an email template with data rendered using email-templates module
-     * check module docs: https://github.com/niftylettuce/node-email-templates
-     *
-     * @param String template path template
-     * @param Object data object with data for use in template
-     */
-    let renderTemplate = (template, data) => {
-        let templateDir = template;
-        let html = new EmailTemplate(templateDir);
-
-        return new Promise((resolve, reject) => {
-            html.render(data, (err, result) => {
-                if (err) {
-                    console.log(err)
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    };
-
-    /**
-     * Parse use this function by default for sends emails
-     * @param mail This object contain to address, subject and email text in plain text
-     * @returns {Promise}
-     */
-    let sendMail = (mail) => {
-        let mailOptions = {
-            to: mail.to,
-            html: mail.text,
-            subject: mail.subject,
-            from: adapterOptions.fromAddress
-        };
-
-        return new Promise((resolve, reject) => {
-
-            if(adapterOptions.service=='SMTP'){
-                transporter.sendMail(mailOptions, (error, info) => {
-                    if(error) {
-                        console.log(error)
-                        reject(error);
-                    } else {
-                        resolve(info);
-                    }
-                });
-            }
-            else if(adapterOptions.service=='Gmail'){
-                transporterOAuth2Gmail.sendMail(mailOptions, (error, info) => {
-                    if(error) {
-                        console.log(error)
-                        reject(error);
-                    } else {
-                        resolve(info);
-                    }
-                });
-            }
-
-        });
-    };
-
-    /**
-     * When this method is available parse use for send email for reset password
-     * @param data This object contain {appName}, {link} and {user} user is an object parse of User class
-     * @returns {Promise}
-     */
-    let sendPasswordResetEmail = (data) => {
-        let mail = {
-            subject: 'Reset Password',
-            to: getUserEmail(data.user)
-        };
-
-        if (adapterOptions.templates && adapterOptions.templates.resetPassword) {
-
-            return renderTemplate(adapterOptions.templates.resetPassword.template, data).then((result) => {
-                mail.text = result.html;
-                mail.subject = adapterOptions.templates.resetPassword.subject;
-
-                return sendMail(mail);
-            }, (e) => {
-
-                return new Promise((resolve, reject) => {
-                    console.log(e)
-                    reject(e);
-                });
-            });
-
-        } else {
-            mail.text = data.link;
-
-            return sendMail(mail);
-        }
-    };
-
-    /**
-     * When this method is available parse use for send email for email verification
-     * @param data This object contain {appName}, {link} and {user} user is an object parse of User class
-     * @returns {Promise}
-     */
-    let sendVerificationEmail = (data) => {
-        let mail = {
-            subject: 'Verify Email',
-            to: getUserEmail(data.user)
-        };
-
-        if (adapterOptions.templates && adapterOptions.templates.verifyEmail) {
-
-            return renderTemplate(adapterOptions.templates.verifyEmail.template, data).then((result) => {
-                mail.text = result.html;
-                mail.subject = adapterOptions.templates.verifyEmail.subject;
-
-                return sendMail(mail);
-            }, (e) => {
-
-                return new Promise((resolve, reject) => {
-                    console.log(e);
-                    reject(e);
-                });
-            });
-
-        } else {
-            mail.text = data.link;
-
-            return sendMail(mail);
-        }
-    };
-
-    return Object.freeze({
-        sendMail: sendMail,
-        renderTemplate: renderTemplate,
-        sendPasswordResetEmail: sendPasswordResetEmail,
-        sendVerificationEmail: sendVerificationEmail
+    return new Promise((resolve, reject) => {
+      if (adapterOptions.service == "SMTP") {
+        transporter.sendMail(mailOptions).then(resolve).catch(reject);
+      } else if (adapterOptions.service == "Gmail") {
+        transporterOAuth2Gmail
+          .sendMail(mailOptions)
+          .then(resolve)
+          .catch(reject);
+      }
     });
+  };
+
+  /**
+   * When this method is available parse use for send email for reset password
+   * @param data This object contain {appName}, {link} and {user} user is an object parse of User class
+   * @returns {Promise}
+   */
+  let sendPasswordResetEmail = (data) => {
+    let mail = {
+      subject: "Reset Password",
+      to: getUserEmail(data.user),
+    };
+
+    if (adapterOptions.templates && adapterOptions.templates.resetPassword) {
+      return renderTemplate(
+        adapterOptions.templates.resetPassword.template,
+        data
+      ).then(
+        (result) => {
+          mail.text = result;
+          mail.subject = adapterOptions.templates.resetPassword.subject;
+
+          return sendMail(mail);
+        },
+        (e) => {
+          return new Promise.reject(e);
+        }
+      );
+    } else {
+      mail.text = data.link;
+
+      return sendMail(mail);
+    }
+  };
+
+  /**
+   * When this method is available parse use for send email for email verification
+   * @param data This object contain {appName}, {link} and {user} user is an object parse of User class
+   * @returns {Promise}
+   */
+  let sendVerificationEmail = (data) => {
+    let mail = {
+      subject: "Verify Email",
+      to: getUserEmail(data.user),
+    };
+
+    if (adapterOptions.templates && adapterOptions.templates.verifyEmail) {
+      return renderTemplate(
+        adapterOptions.templates.verifyEmail.template,
+        data
+      ).then(
+        (result) => {
+          mail.text = result;
+          mail.subject = adapterOptions.templates.verifyEmail.subject;
+
+          return sendMail(mail);
+        },
+        (e) => {
+          return new Promise.reject(e);
+        }
+      );
+    } else {
+      mail.text = data.link;
+
+      return sendMail(mail);
+    }
+  };
+
+  return Object.freeze({
+    sendMail: sendMail,
+    renderTemplate: renderTemplate,
+    sendPasswordResetEmail: sendPasswordResetEmail,
+    sendVerificationEmail: sendVerificationEmail,
+  });
 };
 
 module.exports = SimpleParseSmtpAdapter;
